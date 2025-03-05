a44e223ed9d58de79139e88bcc510240bfe8ff50
Neft <gersonneftaly1@gmail.com>
2025-03-04 21:51:20 -0600

    Agregamos la funcionalidad para ir a la imagen izquierda o derecha, ademas se añadio la descripcion general de la mascota

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
-
-pets = ["assets\pet-images\aniquilator.jpeg",
-        "assets\pet-images\doggy.jpg",
-        "assets\pet-images\luneta.jpeg",
-        "assets\pet-images\mike tyson.jpg",
-        "assets\pet-images\millow.jpeg",
-        "assets\pet-images\whiskers.jpeg"]
+// Array con la url de las imagenes
+const pets = [
+    {name: "Aniquilador",
+    age: 2,
+    image: "assets/pet-images/aniquilator.jpeg",
+    description: "Un poderoso animal que invoca el poder de la ternura"
+    },
+    {name: "Doggy",
+    age: 3,
+    image: "assets/pet-images/doggy.jpg",
+    description: "Un perrito jugueton que adora morder muebles y comer"
+    },
+    {
+        name: "Millow",
+        age: 4,
+        image: "assets/pet-images/millow.jpeg",
+        description: "Millow es amoroso y el perro mas cariñoso del mundo, pero cuidado porque muerde"
+    },
+    {
+        name: "Whiskers",
+        age : 3,
+        image: "assets/pet-images/whiskers.jpeg",
+        description: "Whisker es maldad pura"
+    },
+    {
+        name: "Mordelon",
+        age : 5,
+        image: "assets/pet-images/mordelon.jpeg",
+        description: "El perro con mas energia del mundo y mordidas mas tiernas"
+    }
+];
 
 
 let currentIndex = 0;
 
-
-// Esta funcion se encarga de mostrar la imagen
-
-function updateImage(){
+// actualiza la imagen
+function updatePetInfo() {
     const petImage = document.getElementById("pet-image");
-    petImage.src = pets[currentIndex]; // Aqui se muestra la primera imagen de nuestro array
+    const petAge = document.getElementById("pet-age");
+    const petName = document.getElementById("pet-name");
+    const petDescription = document.getElementById("pet-description");
+
+    petImage.src = pets[currentIndex].image;
+    petName.textContent = pets[currentIndex].name;
+    petDescription.textContent = pets[currentIndex].description;
+    petAge.textContent = pets[currentIndex].age;
 }
 
+
+// Esta funcion muestra la imagen siguiente 
+function nextImage() {
+    currentIndex = (currentIndex + 1) % pets.length;
+    updatePetInfo();
+}
+
+
+// funcion que muestra la imagen previa
+function prevImage() {
+    currentIndex = (currentIndex - 1 + pets.length) % pets.length;
+     updateImage();
+     updatePetInfo();
+}
+
+
+window.onload = updatePetInfo;
